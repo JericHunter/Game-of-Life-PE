9575c970f7454ce21b9022152b3e4808b77c90f9
JericHunter <jeric.hunterr@gmail.com>
2021-07-23 08:17:11 -0700

    conway function finished

--- grid.py
+++ grid.py
@@ -26,3 +26,16 @@ class Grid:
                     pygame.draw.rect(surface, on_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
                 else:
                     pygame.draw.rect(surface, off_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
+        next = np.ndarray(shape=(self.size))
+        if pause == False:
+            for x in range(self.rows):
+                for y in range(self.columns):
+                    state = self.grid_array[x][y]
+                    neighbours = self.get_neighbours( x, y)
+                    if state == 0 and neighbours == 3:
+                        next[x][y] = 1
+                    elif state == 1 and (neighbours < 2 or neighbours > 3):
+                        next[x][y] = 0
+                    else:
+                        next[x][y] = state
+            self.grid_array = next
