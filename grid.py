2ee70994441d17b9b430f3a8a3b5634eed05d9a5
JericHunter <jeric.hunterr@gmail.com>
2021-07-23 08:16:11 -0700

    conway function

--- grid.py
+++ grid.py
@@ -12,3 +12,17 @@ class Grid:
         self.size = (self.rows, self.columns)
         self.grid_array = np.ndarray(shape=(self.size))
         self.offset = offset
+    def random2d_array(self):
+        for x in range(self.rows):
+            for y in range(self.columns):
+                self.grid_array[x][y] = random.randint(0,1)
+
+    def Conway(self, off_color, on_color, surface, pause):
+        for x in range(self.rows):
+            for y in range(self.columns):
+                y_pos = y * self.scale
+                x_pos = x * self.scale
+                if self.grid_array[x][y] == 1:
+                    pygame.draw.rect(surface, on_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
+                else:
+                    pygame.draw.rect(surface, off_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
